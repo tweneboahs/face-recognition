9426d12a0733d9ce44c800d6748babef29fc3224
tweneboahs <tweneboahs@gmail.com>
2022-06-29 13:42:04 -0400

    created logo, rank, signin, form and navigation components to the project

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+const Rank = () => {
+    return (
+        <div >
+            <div className='f3 white'>
+                {'Sarah your current rank is ...'}
+            </div>
+            <div className='f1 white'>
+                {'#6'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
