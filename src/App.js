c68060e1c0a4516e1b2e935ecd97ddc4ab7366ad
tweneboahs <tweneboahs@gmail.com>
2022-07-01 16:26:54 -0400

    1-installed npm clarifai 2-create FaceRecognition component 3-updated button so that when clicked it wld get results about face box from the api 4-created methods calculateFaceLocation and displayFaceBox which find the points on the rect of the face box 5-created FaceRecognition.css in order to draw face box on image

--- src/App.js
+++ src/App.js
@@ -1,109 +1,166 @@
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
+import Clarifai from 'clarifai';
 
-function App() {
-  //particlesInit and optionsParticles are all for the moving background
-  const particlesInit = async (main) => {
-    console.log(main);
+//You must add your own API key here from Clarifai.
+//most api's require some sort of key
+const app = new Clarifai.App({
+  apiKey: '16a0809c6378469fa281853b2ef5bd21'
+ });
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
+//particlesInit and optionsParticles are all for the moving background
+const particlesInit = async (main) => {
+  console.log(main);
 
-  const optionsParticles = {
-    fpsLimit: 120,
-    interactivity: {
-      events: {
-        onClick: {
-          enable: true,
-          mode: "push",
-        },
-        onHover: {
-          enable: true,
-          mode: "repulse",
-        },
-        resize: true,
+  // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+  // starting from v2 you can add only the features you need reducing the bundle size
+  await loadFull(main);
+};
+
+const optionsParticles = {
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
       },
-      modes: {
-        push: {
-          quantity: 4,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
       },
     },
-    particles: {
-      color: {
-        value: "#ffffff",
-      },
-      links: {
-        color: "#ffffff",
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
-        width: 1,
-      },
-      collisions: {
-        enable: false,
-      },
-      move: {
-        direction: "none",
-        enable: true,
-        outModes: {
-          default: "bounce",
-        },
-        random: false,
-        speed: 6,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        value: 80,
-      },
-      opacity: {
-        value: 0,
-      },
-      shape: {
-        type: "circle",
-      },
-      size: {
-        value: { min: 1, max: 5 },
-      },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
     },
-    detectRetina: true,
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: false,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 6,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+}
+
+const particlesLoaded = (container) => {
+  console.log(container);
+};
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input:'',
+      imageUrl:'',
+      box:{},
+    }
   }
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-  return (
-    <div className="App">
-      <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={optionsParticles}
-    />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    //want to get the width and height of the original image so we can use the region info (which will be as a percentage) to draw the regions of the box
+    const width = Number(image.width);
+    const height = Number(image.height);
+    
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+
+
+  }
+
+  displayFaceBox = (box) =>{
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange =(event) =>{
+    this.setState({input:event.target.value});
+  }
+
+  onButtonSubmit = () =>{
+    this.setState({imageUrl: this.state.input});
+    //need to send the input the user wants for face-detection to the API in order to get results back about the region of the face
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input)
+    .then(response =>this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={optionsParticles}
+      />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+    }
 }
 
 
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,17 +1,24 @@
 import React from "react";
 import './ImageLinkForm.css'
 
-
-const ImageLinkForm = () => {
+//remember the onInputChange and onSubmit come from state in App.js
+//use destructuring on the next line so you dont have to use props 
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div className='ma4 mt0'>
             <p className='f3'>
-                {'This magic brain will detect faces'}
+                {'This face recognition app will detect faces in pictures. Give it a try!'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center'type='text'/>
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                    <input 
+                        className='f4 pa2 w-70 center'type='text' 
+                        onChange={onInputChange}
+                    />
+                    <button 
+                        className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' 
+                        onClick={onButtonSubmit}
+                    >Detect</button>
                 </div>
                 
             </div>
